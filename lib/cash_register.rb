7cab56c2bfad1837b45a188879c11cd17718fe18
Kareem Rolle <kareemrolle@yahoo.com>
2020-01-30 17:59:44 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+class CashRegister 
+  attr_accessor :discount, :item, :last_transaction, :total
 
+  def initialize(discount = 0)
+    @items = []
+    @discount = discount
+    @total = 0 
+  end 
+  
+  def add_item(cost, name, number = 1)
+    self.total = cost * number
+    @items << name
+  end
+  
+  def add_item_2(cost, name, number = 1)
+    self.total = cost * number
+
+  def apply_discount 
+  end 
+  
+  def items 
+  end
+  
+  def void_last_transaction
+  end 
+  
+end 
+
+tester = CashRegister.new 
+tester.add_item(4, "carrots", 4)
